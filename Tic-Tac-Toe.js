da9ae61056f39e8087b29818bf8691fbb9ab0142
Yash Agarwal <yash2696@users.noreply.github.com>
2016-01-09 23:49:02 +0530

    changed complete javascript

--- Tic-Tac-Toe.js
+++ Tic-Tac-Toe.js
@@ -1,47 +1,36 @@
-function checkWinner() {
-	var box1 = document.getElementById("#one").innerHTML,
-		box2 = document.getElementById("#two").innerHTML,
-		box3 = document.getElementById("#three").innerHTML,
-		box4 = document.getElementById("#four").innerHTML,
-		box5 = document.getElementById("#five").innerHTML,
-		box6 = document.getElementById("#six").innerHTML,
-		box7 = document.getElementById("#seven").innerHTML,
-		box8 = document.getElementById("#eight").innerHTML,
-		box9 = document.getElementById("#nine").innerHTML;
+turn = "X";
 
-	if(box1 === 'X' && box2 === 'X' && box3 === 'X')
-		alert("X wins");
-	else if(box4 ==='X' && box5 === 'X' && box6 === 'X')
-		alert("X wins");
-	else if(box7 ==='X' && box8 === 'X' && box9 === 'X')
-		alert("X wins");
-	else if(box1 ==='X' && box4 === 'X' && box7 === 'X')
-		alert("X wins");
-	else if(box2 ==='X' && box5 === 'X' && box8 === 'X')
-		alert("X wins");
-	else if(box3 ==='X' && box6 === 'X' && box9 === 'X')
-		alert("X wins");
-	else if(box1 ==='X' && box5 === 'X' && box9 === 'X')
-		alert("X wins");
-	else if(box3 ==='X' && box5 === 'X' && box7 === 'X')
-		alert("X wins");
-	else if(box1 === 'O' && box2 === 'O' && box3 === 'O')
-		alert("O wins");
-	else if(box4 ==='O' && box5 === 'O' && box6 === 'O')
-		alert("O wins");
-	else if(box7 ==='O' && box8 === 'O' && box9 === 'O')
-		alert("O wins");
-	else if(box1 ==='O' && box4 === 'O' && box7 === 'O')
-		alert("O wins");
-	else if(box2 ==='O' && box5 === 'O' && box8 === 'O')
-		alert("O wins");
-	else if(box3 ==='O' && box6 === 'O' && box9 === 'O')
-		alert("O wins");
-	else if(box1 ==='O' && box5 === 'O' && box9 === 'O')
-		alert("O wins");
-	else if(box3 ==='O' && box5 === 'O' && box7 === 'O')
-		alert("O wins");
-	else if(box1.length > 0 && box2.length > 0 && box3.length > 0 && box4.length > 0 && box5.length > 0 && box6.length > 0 && box7.length > 0 && box8.length > 0 && box9.length > 0)
-		alert("Tie");
+function startGame() {
+	setMessage(turn);
+	var nextMove = function() {
+			if(turn = "X") {
+			turn = "O";
+			setMessage(turn);
+			document.innerHTML = turn;
+		}
+
+		else {
+			turn = "X";
+			setMessage(turn);
+			document.innerHTML = turn;
+		}
+	}
+
+	document.getElementById("1").onclick = nextMove;
+	document.getElementById("2").onclick = nextMove;
+	document.getElementById("3").onclick = nextMove;
+	document.getElementById("4").onclick = nextMove;
+	document.getElementById("5").onclick = nextMove;
+	document.getElementById("6").onclick = nextMove;
+	document.getElementById("7").onclick = nextMove;
+	document.getElementById("8").onclick = nextMove;
+	document.getElementById("9").onclick = nextMove;
 }
 
+function setMessage(msg) {
+	document.getElementById("message").innerText = "It's " + msg + "'s turn."; 
+}
+
+window.onload = function () {
+	startGame();
+}
